3da2931540ebfa9ac76f196faaf2a71d072f5585
TUYIMENYE <tuyimenyeaimee@gmail.com>
2024-12-14 04:05:28 -0800

    Updated chatbot project

--- AIMEE/questions_answers.py
+++ AIMEE/questions_answers.py
@@ -1,12 +1,16 @@
 questions_answers = {
-    "What courses are offered?": "We offer various courses in MECHATRONICS, IT, ETT and RENEWABLE ENERGY.",
+    "What courses are offered?": "We offer various courses in MECHATRONICS, IT, ETT, and RENEWABLE ENERGY.",
     "How can I apply?": "You can apply online through our admission portal.",
-    "What is you addmission portal?":"RP MIS" ,
-    "how to pay?":"pay throuth our online portal RP MIS" ,
-    "How many money to pay for registration?":"57 000RWF for registration",
-    "how many lessons?":"It depends on the department, for IT 3 LESSONS, FOR ETT 5 LESSONS" ,
-    "How many years of study?":"3 years for A1 and and we have Btech program for year 4",
-    "Where is the campus located?": "Our campus is located in Tumba in Rulindo district.",
-    "What extracurricular activities are available?": "We have clubs for music, sports, and art."
-    # Add more questions and answers here.
+    "What is your admission portal?": "RP MIS.",
+    "How to pay?": "Pay through our online portal RP MIS.",
+    "How much is the registration fee?": "57,000 RWF for registration.",
+    "How many lessons are there?": "It depends on the department. For IT, there are 3 lessons, and for ETT, 5 lessons.",
+    "How many years of study?": "3 years for A1, and we also have a BTech program for year 4.",
+    "Where is the campus located?": "Our campus is located in Tumba, Rulindo district.",
+    "What extracurricular activities are available?": "We have clubs for music, sports, and art.",
+    "Is there a scholarship program?": "Yes, we offer scholarships based on marks and nkowledge.",
+    "Is there a day program?": "Yes, we have a day program only",
+    "How can I get in touch with an academic advisor?": "You can contact our academic advisors via email or through the student portal.",
+    "Do you offer online courses?": "Yes, we offer online courses in various programs via e-learning",
+    "What are the admission requirements?": "You need a secondary school certificate or equivalent and a passing grade in entrance exams."
 }
